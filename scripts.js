6ff91905d13ca2567ae15b15f6d3684e4a8609e5
Jean-Jacques <162357293+ingjacques@users.noreply.github.com>
2024-06-01 21:44:47 +0000

    Update and rename Scripts.js to scripts.js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// custom-widget.js
+var customWidget = (function() {
+    return {
+        // Initialize the widget
+        init: function() {
+            document.addEventListener('DOMContentLoaded', function() {
+                // Custom JavaScript code here
+                alert('Bienvenue dans le formulaire personnalisé !');
+            });
+        }
+    };
+})();
